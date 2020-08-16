a381179d4596d357fb6fdb0bfb68ecfa46c71216
Radioactive64 <jackie.z.gao@gmail.com>
2020-08-16 19:23:44 -0400

    Create app.c

---
+++ workspace/WRO2020/app.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "ev3api.h"
+#include "app.h"
+#include <unistd.h>
+#include <ctype.h>
+#include <string.h>
+
+#define DEBUG
+
+#ifdef DEBUG
+#define _debug(x) (x)
+#else
+#define _debug(x)
+#endif
+
+// define motors and sensors
+const int color_sensor1 = EV3_PORT_2, color_sensor2 = EV3_PORT_2, color_sensor3 = EV3_PORT_3, color_sensor4 = EV3_PORT_4, left_motor = EV3_PORT_B, right_motor = EV3_PORT_C, a_motor = EV3_PORT_A, d_motor = EV3_PORT_D;
+
+rgb_raw_t rgb1;
+rgb_raw_t rgb4;
+position pos = {-1, -1, -1, 0, 0};
+
+void main_task(intptr_t unused) {
+    init();
+
+    readCode();
+    // TODO run2020
+}
