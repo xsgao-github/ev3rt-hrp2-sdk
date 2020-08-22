eca5a8b532aab263bf2fba4a1447c1eaaecae935
maitian352 <68207888+maitian352@users.noreply.github.com>
2020-08-22 14:10:48 -0400

    Update app.c

--- workspace/WRO2020/app.c
+++ workspace/WRO2020/app.c
@@ -748,20 +748,17 @@ void runRedStreet(){
         wheelDistance = (ev3_motor_get_counts(left_motor) / 2 + ev3_motor_get_counts(right_motor) / 2) * ((3.1415926535 * 8.5) / 360);
     }
     ev3_motor_steer(left_motor,right_motor,-30,0);
-    tslp_tsk(350);
+    tslp_tsk(220);
     ev3_motor_steer(left_motor,right_motor,0,0);
     ev3_motor_set_power(a_motor,-50);
     tslp_tsk(800);
     ev3_motor_set_power(a_motor,0);
-    ev3_motor_steer(left_motor,right_motor,-15,90);
-    tslp_tsk(800);
+    ev3_motor_steer(left_motor,right_motor,-15,65);
+    tslp_tsk(1250);
     ev3_motor_steer(left_motor,right_motor,0,0);
     ev3_motor_steer(left_motor,right_motor,-30,0);
     tslp_tsk(402);
     ev3_motor_steer(left_motor,right_motor,0,0);
-    ev3_motor_reset_counts(left_motor);
-    ev3_motor_reset_counts(right_motor);
-    wheelDistance = 0;
     color4PID(75,1,0);
     ev3_motor_steer(left_motor,right_motor,30,0);
     tslp_tsk(715);
