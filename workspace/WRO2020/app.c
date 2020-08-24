fca83c3881c636e4154358a22accb9f3759a0c45
maitian352 <68207888+maitian352@users.noreply.github.com>
2020-08-24 19:33:21 -0400

    Delete app.cf
    cf = c flat

--- workspace/WRO2020/app.c
+++ workspace/WRO2020/app.c
@@ -858,6 +858,7 @@ void runRedStreet(){
     ev3_motor_steer(left_motor,right_motor,15,-45);
     tslp_tsk(787);
     ev3_motor_steer(left_motor,right_motor,0,0);
+    tslp_tsk(10000000);
     color4PID(60,1,0);
     ev3_motor_steer(left_motor,right_motor,30,0);
     tslp_tsk(550);
