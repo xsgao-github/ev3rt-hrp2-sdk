0395eb1218a3a435de8713e0c7c6e16883650a97
Radioactive64 <jackie.z.gao@gmail.com>
2020-08-25 20:44:14 -0400

    Update app.c

--- workspace/WRO2020/app.c
+++ workspace/WRO2020/app.c
@@ -566,10 +566,10 @@ int round_index = 0;
 
 void main_task(intptr_t unused) {
     init();
-    //readCode();
-    readColorCode();
-    run2020();
-    //runBlueStreet();
+    readCode();
+    //readColorCode();
+    //run2020();
+    runBlueStreet();
 }
 
 void run2020(){
