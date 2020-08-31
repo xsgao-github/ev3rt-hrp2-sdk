8f85fde8e8daffa75878f6d10eb3d4ddfd105ce9
Radioactive64 <jackie.z.gao@gmail.com>
2020-08-30 20:47:55 -0400

    REVERT IF NO WOK

--- workspace/WRO2020/app.c
+++ workspace/WRO2020/app.c
@@ -96,7 +96,7 @@ int allTasks[4][3][7][3] = {
         {
             //index 0
             {
-                10,20,280
+                8,19,280
             },
             //index 1
             {
@@ -108,11 +108,11 @@ int allTasks[4][3][7][3] = {
             },
             //index 3
             {
-                5,8,300
+                15, 1000, 150 // sort of spacer
             },
             //index 4
             {
-                8,20,400
+                2,22,400
             },
             //index 5
             {
@@ -633,52 +633,29 @@ void runBlueStreet(){
     if (instructions.doSnow == 1) {
         linePID_with_tasks(60, 25, false);
         tslp_tsk(100);
-        /* old code
-        ev3_motor_rotate(right_motor, 60, 20, true);
+        ev3_motor_rotate(right_motor, 70, 20, true);
         a_motor_index++;
         ev3_motor_reset_counts(left_motor);
         ev3_motor_reset_counts(right_motor);
-        ev3_motor_steer(left_motor, right_motor, 20, 0);
-        while (((ev3_motor_get_counts(left_motor) + ev3_motor_get_counts(right_motor)) / 2) < 250) {
+        ev3_motor_steer(left_motor, right_motor, 25, 0);
+        while ((((abs(ev3_motor_get_counts(left_motor)) + abs(ev3_motor_get_counts(right_motor))) / 2) * ((3.1415926535 * 8.1) / 360)) < 21) {
             execute_tasks((((abs(ev3_motor_get_counts(left_motor)) + abs(ev3_motor_get_counts(right_motor))) / 2) * ((3.1415926535 * 8.1) / 360)), false);
         }
-        ev3_speaker_play_tone(1000, 50);
-        tslp_tsk(1000);
-        ev3_motor_steer(left_motor, right_motor, 0, 0);
-        tslp_tsk(100);
-        ev3_motor_rotate(right_motor, 30, 20, true);
-        ev3_motor_steer(left_motor, right_motor, 10, -1);
-        while (ev3_color_sensor_get_reflect(color_2) > 20) {
-            display_sensors();
-        }
-        tslp_tsk(250);
-        ev3_motor_steer(left_motor, right_motor, -10, 0);
-        tslp_tsk(250);
-        ev3_motor_rotate(right_motor, 190, 20, true);
-        tslp_tsk(100);
-        a_motor_index++;
-        */
-        ev3_motor_rotate(right_motor, 70, 20, true);
-        ev3_motor_rotate(left_motor, 100, 20, false);
-        ev3_motor_rotate(right_motor, 100, 20, true);
-        ev3_motor_rotate(right_motor, -50, 20, true);
-        ev3_motor_rotate(left_motor, 30, 10, false);
-        ev3_motor_rotate(right_motor, 30, 10, true);
-        ev3_motor_rotate(right_motor, 40, 10, true);
-        a_motor_index++;
-        linePID_with_tasks(4, 20, false);
-        linePID_with_tasks(6, 20, false);
         tslp_tsk(100);
         ev3_motor_steer(left_motor, right_motor, 10, -1);
         while (ev3_color_sensor_get_reflect(color_2) > 20) {
             display_sensors();
         }
-        tslp_tsk(250);
+        tslp_tsk(150);
         ev3_motor_steer(left_motor, right_motor, -10, 0);
-        tslp_tsk(250);
+        ev3_motor_set_power(a_motor, -80);
+        tslp_tsk(150);
         ev3_motor_rotate(right_motor, 180, 20, true);
+        ev3_motor_stop(a_motor, false);
         tslp_tsk(100);
-        linePID_with_tasks(32, 25, false);
+        a_motor_index++;
+        a_task_running = 0;
+        linePID_with_tasks(34, 25, false);
         ev3_motor_set_power(a_motor, -50);
     } else if (instructions.doCar == 1) {
         // TODO: stuff
