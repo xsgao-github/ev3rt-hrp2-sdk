ad9f7203ff929e5d84ec35ee84fdf89772e07d27
Radioactive64 <jackie.z.gao@gmail.com>
2020-09-03 14:54:49 -0400

    lol fixed

--- workspace/WRO2020/app.c
+++ workspace/WRO2020/app.c
@@ -1040,8 +1040,8 @@ void runBlueStreet(){
         ev3_speaker_play_tone(NOTE_G6, -1);
         while (true);
     }
-    ev3_motor_rotate(left_motor, 100, 20, false);
-    ev3_motor_rotate(right_motor, 100, 20, true);
+    ev3_motor_rotate(left_motor, 130, 20, false);
+    ev3_motor_rotate(right_motor, 130, 20, true);
     ev3_motor_stop(a_motor, false);
     tslp_tsk(100);
     ev3_motor_steer(left_motor, right_motor, 30, -40);
